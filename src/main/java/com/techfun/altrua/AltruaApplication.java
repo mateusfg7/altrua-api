a8e4867bee6fa0a472a34734411299c7180f8b05
Mateus Felipe Gonçalves <mateusfelipefg77@gmail.com>
2026-03-11 19:42:08 -0300

    chore: initial commit

---
+++ src/main/java/com/techfun/altrua/AltruaApplication.java
@@ -0,0 +1,13 @@
+package com.techfun.altrua;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AltruaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AltruaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/techfun/altrua/AltruaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.techfun.altrua;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AltruaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
