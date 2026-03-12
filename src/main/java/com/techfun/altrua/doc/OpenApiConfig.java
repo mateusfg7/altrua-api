a9eaf39f37142a5d7e400da499b086a85a5f38e1
Gabriel Henrique <gabrielmkv.dev@gmail.com>
2026-03-12 01:05:21 -0300

    refactor(openapi): improved the custom Swagger

--- src/main/java/com/techfun/altrua/doc/OpenApiConfig.java
+++ src/main/java/com/techfun/altrua/doc/OpenApiConfig.java
@@ -3,6 +3,7 @@ package com.techfun.altrua.doc;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import io.swagger.v3.oas.models.ExternalDocumentation;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
@@ -17,13 +18,19 @@ public class OpenApiConfig {
                 .info(new Info()
                         .title("Altrua API")
                         .version("1.0.0")
-                        .description("Altrua API documentation")
+                        .description("### 🤝 Conectando Doadores e ONGs\n" +
+                                "API para gestão de doações e recrutamento de voluntários.\n\n" +
+                                "**Equipe de Desenvolvimento:**\n" +
+                                "* [Gabriel](https://github.com/gabriel-mkv)\n" +
+                                "* [Mateus](https://github.com/mateusfg7)")
                         .contact(new Contact()
-                                .name("gabriel-mkv")
-                                .email("gabrielmkv.dev@gmail.com"))
-                        .contact(new Contact()
-                                .name("mateusfg7"))
+                                .name("Altrua Team")
+                                .url("https://github.com/mateusfg7/altrua-api"))
                         .license(new License()
-                                .name("GPL-3.0")));
+                                .name("GPL-3.0")
+                                .url("https://www.gnu.org/licenses/gpl-3.0.html")))
+                .externalDocs(new ExternalDocumentation()
+                        .description("Documentação Técnica & Guia de Instalação")
+                        .url("https://github.com/mateusfg7/altrua-api/blob/main/README.md"));
     }
 }
