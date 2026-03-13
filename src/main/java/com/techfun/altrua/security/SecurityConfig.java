bd5e030cd64c75d018f487a3c8350727482aec64
Gabriel Henrique <gabrielmkv.dev@gmail.com>
2026-03-12 23:56:41 -0300

    feat(auth): add Argon2id dependency and config on SecurityConfig

--- src/main/java/com/techfun/altrua/security/SecurityConfig.java
+++ src/main/java/com/techfun/altrua/security/SecurityConfig.java
@@ -5,6 +5,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.argon2.Argon2PasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -25,5 +27,16 @@ public class SecurityConfig {
                         .permitAll()
                         .anyRequest().permitAll());
         return http.build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new Argon2PasswordEncoder(
+            16,   // salt length
+            32,   // hash length
+            1,    // parallelism
+            65536, // memory (64 MB)
+            3     // iterations
+        );
     }
 }
