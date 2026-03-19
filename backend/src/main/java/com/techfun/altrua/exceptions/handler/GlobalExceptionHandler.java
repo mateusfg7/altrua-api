dceb94d6c6b622baa051e1f07bdd5ebce452b369
Gabriel Henrique <gabrielmkv.dev@gmail.com>
2026-03-19 13:49:58 -0300

    feat(exception/api): handle auth exceptions in GlobalExceptionHandler

--- backend/src/main/java/com/techfun/altrua/exceptions/handler/GlobalExceptionHandler.java
+++ backend/src/main/java/com/techfun/altrua/exceptions/handler/GlobalExceptionHandler.java
@@ -1,12 +1,14 @@
 package com.techfun.altrua.exceptions.handler;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import com.techfun.altrua.dto.common.ErrorResponseDTO;
 import com.techfun.altrua.exceptions.EmailAlreadyInUseException;
+import com.techfun.altrua.exceptions.InvalidCredentialsException;
 
 import jakarta.servlet.http.HttpServletRequest;
 
@@ -33,5 +35,33 @@ public class GlobalExceptionHandler {
     @ResponseStatus(HttpStatus.CONFLICT)
     public ErrorResponseDTO handleEmailAlreadyInUse(EmailAlreadyInUseException ex, HttpServletRequest request) {
         return ErrorResponseDTO.of(ex.getMessage(), HttpStatus.CONFLICT, request.getRequestURI());
+    }
+
+    /**
+     * Trata a exceção {@link BadCredentialsException} lançada pelo Spring Security.
+     *
+     * @param ex      a exceção capturada
+     * @param request a requisição HTTP onde a exceção ocorreu
+     * @return um objeto {@link ErrorResponseDTO} com status 401 (Unauthorized) e
+     *         mensagem genérica
+     */
+    @ExceptionHandler(BadCredentialsException.class)
+    @ResponseStatus(HttpStatus.UNAUTHORIZED)
+    public ErrorResponseDTO handleBadCredentials(BadCredentialsException ex, HttpServletRequest request) {
+        return ErrorResponseDTO.of("Credenciais inválidas", HttpStatus.UNAUTHORIZED, request.getRequestURI());
+    }
+
+    /**
+     * Trata a exceção personalizada {@link InvalidCredentialsException}.
+     *
+     * @param ex      a exceção capturada
+     * @param request a requisição HTTP onde a exceção ocorreu
+     * @return um objeto {@link ErrorResponseDTO} com status 401 (Unauthorized)
+     * @see HttpStatus#UNAUTHORIZED
+     */
+    @ExceptionHandler(InvalidCredentialsException.class)
+    @ResponseStatus(HttpStatus.UNAUTHORIZED)
+    public ErrorResponseDTO handleInvalidCredentials(InvalidCredentialsException ex, HttpServletRequest request) {
+        return ErrorResponseDTO.of("Credenciais inválidas", HttpStatus.UNAUTHORIZED, request.getRequestURI());
     }
 }
